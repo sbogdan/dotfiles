e1eca858dfb05b88b868bd59040c6c0502aecafc
sbogdan <bogdan.sachelarie@gmail.com>
2013-02-03 17:50:40 +0000

    added zshrc

--- install.sh
+++ install.sh
@@ -3,8 +3,7 @@ function link_file {
 source="${PWD}/$1"
 target="${HOME}/${1/_/.}"
 
-if [ -e "${target}" ] && [ ! -L "${target}" ]; then
-  mv $target $target.df.bak
+if [ ! -e "${target}" ]; then 
   ln -sf ${source} ${target}
 fi
 
